442670546795107a4e3a1aafb41e011622848ec1
Axel <axelwarneke@gmail.com>
2024-02-10 13:34:00 -0300

    rename

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/chuck-norris-phrase'),
+      dir: require('path').join(__dirname, './coverage/chuckAJoke'),
       subdir: '.',
       reporters: [
         { type: 'html' },
